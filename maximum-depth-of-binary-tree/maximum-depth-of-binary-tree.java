7b6c28de97b3de56c53f974a2c6dcea78852ad92
AQSA AFTAB <aqsabintaftab@gmail.com>
2021-07-13 07:22:01 +0530

    Time: 0 ms (100.00%), Space: 39.2 MB (23.21%) - LeetHub

---
+++ maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int maxDepth(TreeNode root) {
+        
+       int depth =  helper(root);
+        
+        return depth;
+        
+        
+    }
+    
+    
+    public int helper(TreeNode root){
+        
+        if(root == null){
+            return 0;
+        }
+        
+        int left = helper(root.left);
+        int right = helper(root.right);
+        
+        return Math.max(left , right) + 1;
+        
+    }
+}
