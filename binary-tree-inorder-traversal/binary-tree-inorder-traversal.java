99936499e956816a3b45567be280c499ac66bda9
AQSA AFTAB <aqsabintaftab@gmail.com>
2021-07-13 06:05:55 +0530

    Time: 0 ms (100.00%), Space: 37 MB (92.56%) - LeetHub

---
+++ binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
@@ -0,0 +1,55 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        
+        List<Integer> list = new ArrayList<Integer>();
+        
+        helper(root , list);
+        
+        return list;
+       
+        
+        
+    }
+    
+    public void helper(TreeNode root , List<Integer> list){
+        
+        
+        if(root!= null){
+            
+            if(root.left != null){
+                
+                helper(root.left , list);
+                
+            }
+            
+            list.add(root.val);
+            
+            if(root.right != null){
+                
+                helper(root.right , list);
+                
+                
+            }
+            
+            
+        }
+        
+        
+        
+    }
+}
