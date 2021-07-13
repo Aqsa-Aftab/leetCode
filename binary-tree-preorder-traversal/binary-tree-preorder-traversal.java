6a1080537916be26fa3051ab845a40bb7b1dd71b
AQSA AFTAB <aqsabintaftab@gmail.com>
2021-07-13 06:10:30 +0530

    Time: 0 ms (100.00%), Space: 37.5 MB (34.08%) - LeetHub

---
+++ binary-tree-preorder-traversal/binary-tree-preorder-traversal.java
@@ -0,0 +1,55 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        
+        List<Integer> list = new ArrayList<Integer>();
+        
+        helper(root , list);
+        
+        return list;
+        
+        
+    }
+    
+    
+    public  void  helper(TreeNode root , List<Integer> list){
+        
+        if(root != null){
+            
+              list.add(root.val);
+            
+            if(root.left != null){
+                
+                helper(root.left , list);
+            }
+            if(root.right != null){
+                
+                helper(root.right,list);
+            }
+            
+        }
+        
+      
+        
+        
+        
+        
+        
+        
+    }
+    
+}
